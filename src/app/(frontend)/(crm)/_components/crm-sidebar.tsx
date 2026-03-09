fac5d1bd26b93b6f4d4d92d524d3075147165c83
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2026-03-09 12:04:44 +0100

    fix: collapsible goes away

--- src/app/(frontend)/(crm)/_components/crm-sidebar.tsx
+++ src/app/(frontend)/(crm)/_components/crm-sidebar.tsx
@@ -1,9 +1,4 @@
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-} from '@/components/ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/components/ui/sidebar'
 import React from 'react'
 import OasezorgLogo from '../../_components/oasezorg-logo'
 import CrmSidebarMenu from './crm-sidebar-menu'
@@ -11,7 +6,7 @@ import LogoutButton from './logout-button'
 
 export default function CrmSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar variant="floating" collapsible="icon" {...props}>
+    <Sidebar variant="floating" {...props}>
       <SidebarHeader>
         <OasezorgLogo className="h-10 w-auto" />
       </SidebarHeader>
